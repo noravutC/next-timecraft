4d033c51eb29469b965a42bd52a0df7573eb928e
Noravut <noravut.ch@gmail.com>
2025-10-23 17:06:05 +0700

    fixed error utils in example dnd

--- src/app/(main)/project/example/utils.ts
+++ src/app/(main)/project/example/utils.ts
@@ -1,23 +1,23 @@
-import { Active, DataRef, Over } from "@dnd-kit/core";
-import { ColumnDragData } from "./board-column";
-import { TaskDragData } from "./task-card";
+// import { Active, DataRef, Over } from "@dnd-kit/core";
+// import { ColumnDragData } from "./board-column";
+// import { TaskDragData } from "./task-card";
 
-type DraggableData = ColumnDragData | TaskDragData;
+// export type DraggableData = ColumnDragData | TaskDragData;
 
-export function hasDraggableData<T extends Active | Over>(
-  entry: T | null | undefined
-): entry is T & {
-  data: DataRef<DraggableData>;
-} {
-  if (!entry) {
-    return false;
-  }
+// export function hasDraggableData<T extends Active | Over>(
+//   entry: T | null | undefined
+// ): entry is T & {
+//   data: DataRef<DraggableData>;
+// } {
+//   if (!entry) {
+//     return false;
+//   }
 
-  const data = entry.data.current;
+//   const data = entry.data.current;
 
-  if (data?.type === "Column" || data?.type === "Task") {
-    return true;
-  }
+//   if (data?.type === "Column" || data?.type === "Task") {
+//     return true;
+//   }
 
-  return false;
-}
+//   return false;
+// }
