e09abb88bd391df31a8ed5c5404427d204a803d5
Noravut <noravut.ch@gmail.com>
2025-11-18 22:51:09 +0700

    force fixed error deploy

---
+++ src/app/(main)/project/project-tools-layout.tsx
@@ -0,0 +1,51 @@
+// project/project-tools-layout.tsx
+'use client';
+
+import React, { useEffect } from "react";
+// components
+import { TabProject } from "./components/tab-project";
+// hooks
+import { useProjectStore } from "@/hooks";
+// utils
+import { ProjectTabProvider } from "./context/project-menu-context";
+import { LoaderPage } from "@/components/Loader-page";
+import { ProjectToolsMenu } from "./components/project-tools/project-tools";
+
+export const ProjectToolsLayout = ({ children }: { children: React.ReactNode })  => {
+    const {
+        fetchProjects,
+        status,
+    } = useProjectStore();
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    return (
+        <ProjectTabProvider>
+            <div className="relative max-w-full w-full h-full overflow-x-hidden flex flex-col">
+                <div className="max-h-[60px] min-h-[60px] w-full flex justify-start items-center overflow-hidden">
+                    <div className="flex flex-col w-full">
+                        <div className="flex items-center gap-4 pl-4">
+                            {/* Top menu tools */}
+                            <ProjectToolsMenu />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1 max-h-flex-1 overflow-hidden">
+                    {status === 'fetching' ? (
+                        <div className='h-full w-full pt-2 flex justify-center items-center'>
+                            <LoaderPage ballSize={3} />
+                        </div>
+                    ) : (
+                        <>
+                            {children}
+                        </>
+                    )}
+                </div>
+                {/* Bottom menu tools */}
+                <TabProject />
+            </div>
+        </ProjectTabProvider>
+    )
+}
