62a8c9291b6eb5285c3db04237ae8ebded96cc1b
Noravut <noravut.ch@gmail.com>
2025-11-23 22:23:50 +0700

    update layout

--- src/app/(main)/project/page.tsx
+++ src/app/(main)/project/page.tsx
@@ -1,53 +1,24 @@
-// // projects/page.tsx
-// "use client";
-
-// import React, { useState, useEffect, useMemo } from "react";
-// // hooks
-// import { useProjectStore } from '@/hooks/useProjects.hook';
-// // components
-// import { KanbanBoard } from "./components/kanban/kanban-board";
-// // import { TemplateColumnForm } from "./components/sub-create-project/template-column-form";
-// // context
-// import { useProjectAtMenu } from "./context/project-menu-context";
-// import { ProjectToolsLayout } from "./project-tools-layout";
-// // types
-// // import { LoaderPage } from "@/components/Loader-page";
-
-// export default function Projects() {
-//   const { projectIdActivate } = useProjectStore();
-//   const { menuValue } = useProjectAtMenu();
-//   return (
-//     <ProjectToolsLayout>
-//       {menuValue === "Board" && (
-//         <KanbanBoard projectId={projectIdActivate} />
-//       )}
-//       {/* {menuValue === "Board Templates" && (
-//         <TemplateColumnForm projectId={projectIdActivate} />
-//       )} */}
-//     </ProjectToolsLayout>
-//   )
-// }
-// projects/page.tsx
+//projects/page.tsx
 "use client";
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 import React from "react";
 import { useProjectStore } from '@/hooks/useProjects.hook';
 import { KanbanBoard } from "./components/kanban/kanban-board";
 import { useProjectAtMenu } from "../../../context/project/project-menu-context";
-import { ProjectProvider } from "@/context/project-tab";
 
 export default  function ProjectsContent() {
   const { projectIdActivate } = useProjectStore();
   const { menuValue } = useProjectAtMenu();
 
   return (
-    <ProjectProvider>
+    <>
 
       {menuValue === "Board" && (
         <KanbanBoard projectId={projectIdActivate} />
       )}
-    </ProjectProvider>
+    </>
   );
 }
