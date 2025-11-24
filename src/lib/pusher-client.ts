dba15cd8e5373714528561866ec28717fb09ce44
Noravut <noravut.ch@gmail.com>
2025-11-24 22:20:59 +0700

    add log to see when deploy

--- src/lib/pusher-client.ts
+++ src/lib/pusher-client.ts
@@ -3,6 +3,10 @@
 
 import Pusher from "pusher-js";
 // Use for client side only
+if (typeof window !== 'undefined') {
+  console.log("Pusher Key Check:", process.env.NEXT_PUBLIC_PUSHER_KEY ? "Found ✅" : "Missing ❌");
+}
+
 export const pusherClient = typeof window !== 'undefined' 
   ? new Pusher(
       process.env.NEXT_PUBLIC_PUSHER_KEY!,
